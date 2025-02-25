24ea3165482c7b96a31c974f0363a080b2180408
widi9545 <widi9545@colorado.edu>
2025-02-24 19:40:49 -0700

    test push

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, widi9545\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, wiid9545\n");
 }
 
 module_init(hello_init);
